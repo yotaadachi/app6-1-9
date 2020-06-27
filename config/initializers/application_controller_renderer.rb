2d687f357ebdccb764e8f9727d79d2d2e8a24a76
安達陽太 <yota@MacBook-Pro.local>
2020-06-27 23:19:14 +0900

    Start

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6a06ed795662cf86661dd38bebec4a0ed9d74c576e06e7f3da6fa0dfbaebba025d91043f643e1fd231b92be2a575e336753d27f733515fcecfba927f28fd3c80'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root 'post_images#index'
+
+  resources :post_images, only: [:new, :create, :index, :show] do
+  	resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+end
